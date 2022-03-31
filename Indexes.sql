4553d74cad4283f182ea032e86df4732899f056e
ArifBadhon <arif14pcc@gmail.com>
2022-03-31 04:16:05 +0100

    Create Indexes.sql

---
+++ Indexes.sql
@@ -0,0 +1,16 @@
+USE dndtech;
+/*
+CREATE CLUSTERED INDEX idx_CustomerFristName
+ON Customers (FirstName);
+
+DROP INDEX idx_CustomerFristName
+ON Customers;
+*/
+----------------------Can not create more than one clastered index in one table-------
+-------------------------------   PK is clastered Index--------------------------------
+
+CREATE NONCLUSTERED INDEX idx_CustName
+ON Customers(LastName);
+
+CREATE NONCLUSTERED COLUMNSTORE INDEX idx_SuppNmCity
+ON Suppliers(SupplierName, SupplierCity);
